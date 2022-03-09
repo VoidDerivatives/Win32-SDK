4292318ba1e75d7d36fa14015f85a59d47a9d937
Aishwarya Bacche <89602581+VoidDerivatives@users.noreply.github.com>
2022-03-09 03:19:10 -0500

    Paint Message Through Program

---
+++ 10_paintMessgaeThroughProgram/paintMessage.c
@@ -0,0 +1,98 @@
+#include<windows.h>
+
+LRESULT CALLBACK WndProc(HWND,UINT,WPARAM,LPARAM);
+
+int WINAPI WinMain(
+					HINSTANCE hInstance,
+					HINSTANCE hPrecInstance,
+					LPSTR     pszCmdLine,
+					int      iCmdShow
+					)
+{
+	MSG msg;
+	WNDCLASSEX wndclass;
+	BOOL bRet;
+	HWND hwnd;
+
+	TCHAR szAppName[] = TEXT("CenterWindow");
+
+	wndclass.cbSize = sizeof(wndclass);
+
+	wndclass.hInstance = hInstance;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.lpszClassName = szAppName;
+
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.hIcon = LoadIcon(NULL,IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL,IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH) GetStockObject(WHITE_BRUSH);
+	wndclass.lpszMenuName = NULL;
+	wndclass.cbClsExtra = 0;
+	wndclass.hIconSm = LoadIcon(NULL,IDI_APPLICATION);
+
+	if(!RegisterClassEx(&wndclass))
+	{
+		MessageBox(NULL,TEXT("RegisterClassEx : FalLURE"),szAppName,0);
+		return -1;
+	} 
+
+	hwnd = CreateWindow(
+					szAppName,
+					TEXT("First Window"),
+					WS_OVERLAPPEDWINDOW,
+					CW_USEDEFAULT,
+					CW_USEDEFAULT,
+					CW_USEDEFAULT,
+					CW_USEDEFAULT,
+					NULL,
+					NULL,
+					hInstance,
+					NULL
+					);
+
+	if(hwnd == NULL)
+	{
+		MessageBox(hwnd,TEXT("CreateWindow : FalLURE"),szAppName,0);
+		return -1;	
+	}
+
+	ShowWindow(hwnd,iCmdShow);
+	UpdateWindow(hwnd);
+
+	while((bRet = GetMessage(&msg,NULL,0,0)) != 0)
+	{
+		if(bRet == -1)
+		{
+			MessageBox(NULL,TEXT("CreateWindow : FalLURE"), szAppName , 0);
+			return -1;
+		}
+		TranslateMessage(&msg);
+
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;;
+}
+LRESULT CALLBACK WndProc(HWND hwnd,
+						UINT uiMessage,
+						WPARAM wParam,
+						LPARAM lParam)
+{
+	PAINTSTRUCT ps;
+	HDC hdc;
+
+	switch(uiMessage)
+	{
+	case WM_PAINT:
+			hdc  = BeginPaint(hwnd,&ps);
+
+			EndPaint(hwnd,&ps);
+			break;
+		
+	case WM_DESTROY:
+				PostQuitMessage(0);
+				break;
+
+	}
+	return DefWindowProc(hwnd,uiMessage,wParam,lParam);
+}
+
