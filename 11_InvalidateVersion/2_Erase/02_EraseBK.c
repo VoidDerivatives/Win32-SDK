06a9e3b89da9cf44a4b6c4932a62d201a34f7733
Aishwarya Bacche <89602581+VoidDerivatives@users.noreply.github.com>
2022-03-13 12:51:59 -0400

    Add files via upload

---
+++ 11_InvalidateVersion/2_Erase/02_EraseBK.c
@@ -0,0 +1,125 @@
+#include<windows.h>
+#include<tchar.h>
+
+LRESULT CALLBACK WndProc(HWND,UINT,WPARAM,LPARAM);
+
+#define WINDOW_WIDTH	800
+#define WINDOW_HEIGHT   600
+
+int WINAPI WinMain(
+					HINSTANCE hInstance,
+					HINSTANCE hPrecInstance,
+					LPSTR     pszCmdLine,
+					int      iCmdShow
+					)
+{
+	MSG msg;
+	WNDCLASSEX wndclass;
+	BOOL bRet;
+	HWND hwnd;
+
+	int cxScreen;
+	int cyScreen;
+
+	int x;
+	int y;
+
+	TCHAR szAppName[] = TEXT("CenterWindow");
+
+	wndclass.cbSize = sizeof(wndclass);
+
+	wndclass.hInstance = hInstance;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.lpszClassName = szAppName;
+
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.hIcon = LoadIcon(NULL,IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL,IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH) GetStockObject(WHITE_BRUSH);
+	wndclass.lpszMenuName = NULL;
+	wndclass.cbClsExtra = 0;
+	wndclass.hIconSm = LoadIcon(NULL,IDI_APPLICATION);
+
+	if(!RegisterClassEx(&wndclass))
+	{
+		MessageBox(NULL,TEXT("RegisterClassEx : FalLURE"),szAppName,0);
+		return -1;
+	} 
+
+	cxScreen = GetSystemMetrics(SM_CXSCREEN);
+	cyScreen = GetSystemMetrics(SM_CYSCREEN);
+
+	x = ( cxScreen / 2 ) - (WINDOW_WIDTH / 2);
+	y = ( cyScreen / 2 ) - (WINDOW_HEIGHT / 2);
+	hwnd = CreateWindow(
+					szAppName,
+					TEXT("First Window"),
+					WS_OVERLAPPEDWINDOW,
+					x,
+					y,
+					WINDOW_WIDTH,
+					WINDOW_HEIGHT,
+					NULL,
+					NULL,
+					hInstance,
+					NULL
+					);
+
+	if(hwnd == NULL)
+	{
+		MessageBox(hwnd,TEXT("CreateWindow : FalLURE"),szAppName,0);
+		return -1;	
+	}
+
+	ShowWindow(hwnd,iCmdShow);
+	UpdateWindow(hwnd);
+
+	while((bRet = GetMessage(&msg,NULL,0,0)) != 0)
+	{
+		if(bRet == -1)
+		{
+			MessageBox(NULL,TEXT("CreateWindow : FalLURE"), szAppName , 0);
+			return -1;
+		}
+		TranslateMessage(&msg);
+
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
+LRESULT CALLBACK WndProc(HWND hwnd,
+						UINT uiMessage,
+						WPARAM wParam,
+						LPARAM lParam)
+{
+	PAINTSTRUCT ps;
+	static RECT rect;
+	HDC hdc;
+
+	switch(uiMessage)
+	{
+	case WM_PAINT:
+			hdc  = BeginPaint(hwnd,&ps);
+
+			TextOut(hdc,rect.left,rect.top,TEXT("Hello World"),(int)_tcslen(TEXT("Hello World")));
+
+			EndPaint(hwnd,&ps);
+			break;
+		
+	case WM_LBUTTONDOWN:
+			rect.left = 100;
+			rect.top = 100;
+			rect.right = 100;
+			rect.bottom = 100;
+
+			InvalidateRect(hwnd,&rect,FALSE);
+			break;	
+
+	case WM_DESTROY:
+				PostQuitMessage(0);
+				break;
+
+	}
+	return DefWindowProc(hwnd,uiMessage,wParam,lParam);
+}
+
