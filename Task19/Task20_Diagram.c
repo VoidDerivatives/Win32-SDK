d79d95ef78b33c42ef93b346f0b429e2cd2b17ca
Aishwarya Bacche <89602581+VoidDerivatives@users.noreply.github.com>
2022-04-03 18:13:58 +0530

    Add files via upload

---
+++ Task19/Task20_Diagram.c
@@ -0,0 +1,145 @@
+#include<windows.h>
+#include<stdio.h>
+#include<tchar.h>
+
+#pragma comment(lib,"user32.lib")
+#pragma comment(lib,"kernel32.lib")
+#pragma comment(lib,"gdi32.lib")
+
+LRESULT CALLBACK WndProc(HWND,UINT,WPARAM,LPARAM);
+
+#define WINDOW_WIDTH	800
+#define WINDOW_HEIGHT   600
+
+int WINAPI WinMain(
+					HINSTANCE hInstance,
+					HINSTANCE hPrecInstance,
+					LPSTR     pszCmdLine,
+					int      iCmdShow
+					)
+{
+	MSG msg;
+	WNDCLASSEX wndclass;
+	BOOL bRet;
+	HWND hwnd;
+
+	int cxScreen;
+	int cyScreen;
+
+	int x;
+	int y;
+
+	TCHAR szAppName[] = TEXT("CenterWindow");
+
+	wndclass.cbSize = sizeof(wndclass);
+
+	wndclass.hInstance = hInstance;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.lpszClassName = szAppName;
+
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.hIcon = LoadIcon(NULL,IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL,IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH) GetStockObject(WHITE_BRUSH);
+	wndclass.lpszMenuName = NULL;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hIconSm = LoadIcon(NULL,IDI_APPLICATION);
+
+	if(!RegisterClassEx(&wndclass))
+	{
+		MessageBox(NULL,TEXT("RegisterClassEx : FAlLURE"),szAppName,0);
+		return -1;
+	} 
+
+	cxScreen = GetSystemMetrics(SM_CXSCREEN);
+	cyScreen = GetSystemMetrics(SM_CYSCREEN);
+
+	x = ( cxScreen / 2 ) - (WINDOW_WIDTH / 2);
+	y = ( cyScreen / 2 ) - (WINDOW_HEIGHT / 2);
+	hwnd = CreateWindow(
+					szAppName,
+					TEXT("COMBO USE"),
+					WS_OVERLAPPEDWINDOW,
+					x,
+					y,
+					WINDOW_WIDTH,
+					WINDOW_HEIGHT,
+					NULL,
+					NULL,
+					hInstance,
+					NULL
+					);
+
+	if(hwnd == NULL)
+	{
+		MessageBox(hwnd,TEXT("CreateWindow : FAlLURE"),szAppName,0);
+		return -1;	
+	}
+
+	ShowWindow(hwnd,iCmdShow);
+	UpdateWindow(hwnd);
+
+	while((bRet = GetMessage(&msg,NULL,0,0)) != 0)
+	{
+		if(bRet == -1)
+		{
+
+			MessageBox(NULL,TEXT("CreateWindow : FAlL;/?URE"), szAppName , 0);
+			return -1;
+		}
+		TranslateMessage(&msg);
+
+		DispatchMessage(&msg);
+	}
+	return (int)msg.wParam;
+}
+
+#define MY_ARRAY_SIZE(x) (sizeof(x))/(sizeof(x[0]))
+
+LRESULT CALLBACK WndProc(HWND hwnd,
+						UINT uiMessage,
+						WPARAM wParam,
+						LPARAM lParam)
+{
+	HDC hdc;
+	RECT rect;
+	PAINTSTRUCT ps;
+
+	static int cxClient;
+	static int cyClient;
+	
+	switch(uiMessage)
+	{		
+	case WM_SIZE:
+
+			cxClient = LOWORD(lParam);
+			cyClient = HIWORD(lParam);
+		
+			break;
+	case WM_PAINT:
+			hdc  = BeginPaint(hwnd,&ps);
+
+			Rectangle(hdc,cxClient/8,cyClient/8,7*cxClient/8,7*cyClient/8);
+
+			LineTo(hdc,cxClient,cyClient);
+
+			MoveToEx(hdc,0,cyClient,NULL);
+
+			LineTo(hdc,cxClient,0);
+
+			Ellipse(hdc,cxClient/8,cyClient/8,7*cxClient/8,7*cyClient/8);
+
+			RoundRect(hdc,cxClient/4,cyClient/4,3*cxClient/4,3*cyClient/4,cxClient/4,cyClient/4);
+
+			EndPaint(hwnd,&ps);
+			
+			break;
+	
+	case WM_DESTROY:
+				PostQuitMessage(0);
+			break;
+
+	}
+	return DefWindowProc(hwnd,uiMessage,wParam,lParam);
+}
